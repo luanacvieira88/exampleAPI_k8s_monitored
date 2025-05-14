765a8457b85317c3944e5c1d7abcd180cee9c039
Luana Corrêa Vieira <luana.correa.vieira@avanade.com>
2025-05-14 17:01:54 -0300

    Firts files of projects

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  description = "Nome do grupo de recursos"
+  default     = "example-resources"
+}
+
+variable "location" {
+  description = "Localização do recurso"
+  default     = "East US"
+}
